03c473612037d487d9b89facf3d03cc185c674bc
Alina-mughal <49423581+Alina-mughal@users.noreply.github.com>
2019-05-01 22:27:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.write("<h3>Personal Profile</h3><br>Name: Alina<br>Age: 23<br>Address: Pasrur Sialkot<br>Telephone Number: +92 300 6177917<br>E.mail:aylee.alina@gmail.com<br>Domicile: Sialkot<br>Marital Status: Single");
+document.write("<h3>Career Objectives:</h3>");
+document.write("Seeking a position in an organization to utilize my skills and build a good rapport in the company and take them to a new height.");
+document.write("<h3>Education:</h3>");
+document.write("2015-2019 <br>  software engineer     University of Gujrat, Sialkot Campus, Sialkot.");     
+
+document.write("<br>2014 <br>  FSC (pre engineering) <br>  superior college pasrur<br> B");
+document.write("<br>2012 <br>   HSSC (Science)    <br>  Government Girls High school Pasrur, Sialkot. <br> B");
+document.write("<h3>Experience</h3>Fresher");
+document.write("<h3>Key Skills and Attributes</h3><br>Decision making and problem solving<br>Persuasive & Team leader<br>Microsoft office<br>Visio <br>Visual studio <br>Android studio<br>Computer and internet skills");
+document.write("<h3>Interests and Hobbies</h3><br> cycling<br>Poetry Writing<br>	Novel Reading<br>	Movies<br>	Research");
+document.write("<h3>Languages</h3><br>English<br>Urdu<br>Punjabi");
